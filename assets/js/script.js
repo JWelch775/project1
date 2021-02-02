6cef22c2578be6b58b62b5eb9357b4e5f58e74bc
jojoford <75343703+jojoford@users.noreply.github.com>
2021-02-01 23:33:15 -0600

    changed style and added lyric search api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-var artistFormEl = document.querySelector("#artist-form")
-var nameInputEl = document.querySelector("#artist")
-var cityInputEl = document.querySelector("#citySearch")
-var eventContainerEl = document.querySelector("event-container")
-
-
+var searchFormEl = document.querySelector("#user-form")
+var artistInputEl = document.querySelector("#artist-search")
+var eventContainerEl = document.querySelector("#event-container")
+var artistSearchTerm = document.querySelector("#event-search-term")
 
 function getEvents(keyword)
     {
@@ -15,9 +13,10 @@ function getEvents(keyword)
             {
                 if(response.ok)
                     {
-                        response.json().then(function(data)
+                        response.json().then(function(response)
                             {
-                                console.log(data, keyword);
+                                displayEvents(response, keyword);
+                                //console.log(keyword, response);
                             });
                     }
                 else(function(error)
@@ -42,10 +41,6 @@ function formSubmitHandler(event)
             {
                 getEvents(artist);
                 artistInputEl.value = "";
-
-                // clear old content
-      eventContainerEl.textContent = "";
-      nameInputEl.value = "";
             }
         else
             {
@@ -53,19 +48,143 @@ function formSubmitHandler(event)
             }
     };
 
-
-function displayEvents()
+function displayEvents(shows, searchTerm)
     {
+        var showArr = shows._embedded.events
+        console.log(shows);
+        eventContainerEl.textContent = "";
+        artistSearchTerm.textContent = searchTerm;
+        
 
+
+        //check if api returned any events
+        if(showArr.length === 0)
+            {
+                eventContainerEl.textContent = "No events found for this artist";
+                return;
+            }
+
+        //loop over events
+        for(var i = 0; i < showArr.length; i++)
+            {
+                var eventTitle = showArr[i].name;
+                console.log(eventTitle);
+
+                //create a container for each event
+                var eventEl = document.createElement("a");
+                eventEl.classList = "list-item";
+                
+                //create a span element to hold event names
+                var eventTitleEl = document.createElement("span");
+                eventTitleEl.textContent = eventTitle;
+
+                //append to container 
+                eventEl.appendChild(eventTitleEl);
+
+                eventContainerEl.appendChild(eventEl);
+
+            }
+            
+            
     }
 
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
 
 
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
 
+const apiURL = 'https://api.lyrics.ovh';
 
+// Search by song or artist
+async function searchSongs(term) {
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
 
+    showData(data);
+}
+
+// Show song and artist in DOM
+function showData(data) {
+    result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+            .map(
+                song => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`
+            )
+            .join('')}
+    </ul>
+  `;
+
+    if (data.prev || data.next) {
+        more.innerHTML = `
+      ${
+            data.prev
+                ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+                : ''
+            }
+      ${
+            data.next
+                ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+                : ''
+            }
+    `;
+    } else {
+        more.innerHTML = '';
+    }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await res.json();
+
+    showData(data);
+}
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+    more.innerHTML = '';
+}
+
+// Event listeners
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+    if (!searchTerm) {
+        alert('Please type in a search term');
+    } else {
+        searchSongs(searchTerm);
+    }
+});
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, songTitle);
+    }
+});
 
 
 
